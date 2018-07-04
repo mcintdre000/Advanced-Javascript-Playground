02544632f346f14a735b125cf9a285d2acdd5969
mcintdre000 <38018234+mcintdre000@users.noreply.github.com>
2018-07-04 12:01:32 -0700

    Used setState callback

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    }
+  }
+
+  increment = () => {
+    this.setState((prevState) => {
+      return {counter: prevState.counter + 1 }
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={() => this.increment(this.state.counter)}>Increment</button>
+        {this.state.counter}
       </div>
     );
   }
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-function double(x) {
-    if(!Number.isInteger(x))
-    throw new Error(`${x} is not an integer!`)
-    return x * 2;
-}
+// function double(x) {
+//     if(!Number.isInteger(x))
+//     throw new Error(`${x} is not an integer!`)
+//     return x * 2;
+// }
 
-const result = plus5(double('5'));
+// // const result = plus5(double('5'));
+// const result = plus5(double(5));
 
-console.log('-------- result', result);
+// console.log('-------- result', result);
