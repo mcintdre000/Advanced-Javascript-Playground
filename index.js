7afd97c08144704953e70aaae3365d68891aa057
mcintdre000 <38018234+mcintdre000@users.noreply.github.com>
2018-07-04 11:23:41 -0700

    Error handling

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// hello
+function double(x) {
+    if(!Number.isInteger(x))
+    throw new Error(`${x} is not an integer!`)
+    return x * 2;
+}
+
+const result = plus5(double('5'));
+
+console.log('-------- result', result);
